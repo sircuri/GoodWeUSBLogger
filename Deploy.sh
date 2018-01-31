be1c0a3759b8fe6efe6a0f0e211ceacb86a80951
Arjen van Efferen <arjen@vanefferenonline.nl>
2018-01-31 20:14:00 +0100

    Update Deploy.sh to not manually copy to target folder

--- Deploy.sh
+++ Deploy.sh
@@ -2,6 +2,7 @@
 
 # sudo apt-get update
 installpath=$(get_octopusvariable "Octopus.Action.Package.InstallationDirectoryPath")
+targetpath=$(get_octopusvariable "Application.Installation.Path")
 
 sudo python -m pip install pyudev
 sudo python -m pip install ioctl_opt
@@ -9,12 +10,16 @@ sudo python -m pip install configparser
 sudo python -m pip install paho-mqtt
 sudo python -m pip install enum
 
-echo "Remove symlink if previously created"
-if [ -e ~/octopus ]; then
-  rm -rf ~/octopus
-fi
+echo "Create installation folder $targetpath"
+sudo mkdir -p "$targetpath"
 
-echo "Make 'GoodWe.py' script executable"
-chmod +x "${installpath}/GoodWe.py"
-echo "Make symlink from ${installpath} to ~/octopus"
-ln -s "${installpath}" ~/octopus
+echo "Purge installation folder $targetpath"
+sudo rm -rf $targetpath/*
+
+echo "Copy application from $installpath to $targetpath"
+sudo cp -r $installpath/* $targetpath
+echo "Add executable flag on $targetpath/GoodWe.py"
+sudo chmod +x $targetpath/GoodWe.py
+
+echo "Move ./etc/goodwe.conf to /etc"
+sudo mv -f $installpath/etc/goodwe.conf /etc
