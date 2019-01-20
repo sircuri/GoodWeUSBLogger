2739831659ee631d413575586891c8afa9cccc32
Arjen van Efferen <arjen@vanefferenonline.nl>
2019-01-20 14:07:19 +0100

    Deployment based on GitLab CI and no longer on Octopus Deploy

--- deploy/stop-services.sh
+++ deploy/stop-services.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Verify pre-conditions for deployment
-stepname=$(get_octopusvariable "Application.Installation.UploadStepName")
-installpath=$(get_octopusvariable "Octopus.Action[$stepname].Output.Package.InstallationDirectoryPath")
-targetpath=$(get_octopusvariable "Application.Installation.Path")
-
-echo "Stepname: $stepname"
-echo "Installpath: $installpath"
-echo "Targetpath: $targetpath"
-
-if [ -z "$stepname" ] || [ -z "$installpath" ] || [ -z "$targetpath" ]
-then
-	fail_step "Missing some required system variables"
-fi
-
 if ps aux | grep '[r]estartd'; then
     echo "Stop Restartd daemon"
     sudo service restartd stop
