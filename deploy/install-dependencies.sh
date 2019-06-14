816721f5817b4f601ca1b902f2474048f80c743a
Arjen van Efferen <arjen@vanefferenonline.nl>
2019-06-14 19:17:27 +0200

    Put python packages on a single line

--- deploy/install-dependencies.sh
+++ deploy/install-dependencies.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 echo "Install Python dependencies"
-sudo python -m pip install pyudev
-sudo python -m pip install ioctl_opt
-sudo python -m pip install configparser
-sudo python -m pip install paho-mqtt
-sudo python -m pip install enum
+sudo python -m pip install configparser paho-mqtt pyudev ioctl_opt enum
