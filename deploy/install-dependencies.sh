b4030f3e0423a705633ff9c7ac5c0dae76bdd1a2
Arjen van Efferen <arjen@vanefferenonline.nl>
2019-08-10 14:31:21 +0200

    Correct install scripts

--- deploy/install-dependencies.sh
+++ deploy/install-dependencies.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Install Python dependencies"
-sudo python -m pip install configparser paho-mqtt pyudev ioctl_opt enum
+sudo python -m pip install configparser paho-mqtt pyudev ioctl_opt simplejson
+sudo apt-get -y install --reinstall python-enum34
